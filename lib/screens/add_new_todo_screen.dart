d29fa83cf35d5e0e7cb443499c3295797bdc1634
IstiakPolok <fatin15-1944@diu.edu.bd>
2024-12-15 22:57:02 +0600

    commit

---
+++ lib/screens/add_new_todo_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/screens/todo.dart';
+
+class AddNewTodoScreen extends StatefulWidget {
+  const AddNewTodoScreen({super.key});
+
+  @override
+  State<AddNewTodoScreen> createState() => _AddNewTodoScreenState();
+}
+
+final TextEditingController _titleTEController = TextEditingController();
+final TextEditingController _descriptionTEController = TextEditingController();
+final GlobalKey<FormState> _fromKey = GlobalKey<FormState>();
+
+class _AddNewTodoScreenState extends State<AddNewTodoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Add new todo"),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _fromKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _titleTEController,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: const InputDecoration(
+                        labelText: "Title", hintText: 'Write Your Todo Title'),
+                    validator: (String? value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return "Enter Your Title";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    controller: _descriptionTEController,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    maxLines: 3,
+                    decoration: const InputDecoration(
+                        labelText: "Description",
+                        hintText: 'Write Your Description here'),
+                    validator: (String? value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return "Enter Your Description";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_fromKey.currentState!.validate()) {
+                        Todo todo = Todo(
+                            title: _titleTEController.text.trim(),
+                            description: _descriptionTEController.text.trim());
+                        Navigator.pop(context, todo);
+                      }
+                    },
+                    child: const Text("ADD"),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
